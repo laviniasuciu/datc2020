52481a3a12775aed1b6ff636bb43e1d12e1b3a82
laviniasuciu <laviniasuciu16@gmail.com>
2020-10-12 12:02:01 +0300

    Tema3

---
+++ Suciu-Lavinia/L03/DriveApiService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace L03
+{
+    public class DriveApiService
+    {
+        protected static string[] scopes = { DriveService.Scope.Drive };
+        protected readonly UserCredential credential;
+        static string ApplicationName = "MyApplicationName";
+        protected readonly DriveService service;
+        protected readonly FileExtensionContentTypeProvider fileExtensionProvider;
+
+        public DriveApiService()
+        {
+            string YOUR_GMAIL_EMAIL = "laviniasuciu16@gmail.com";
+            using (var stream =
+                new FileStream("client_id.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    scopes,
+                    YOUR_GMAIL_EMAIL, // use a const or read it from a config file
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+
+                fileExtensionProvider = new FileExtensionContentTypeProvider();
+            }
+
+            service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+    }
+}
