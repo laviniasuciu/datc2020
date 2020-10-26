b8125a2b4228e7d39541fcbcc47ae1d9d32addc3
laviniasuciu <laviniasuciu16@gmail.com>
2020-10-26 15:18:17 +0200

    Tema5

---
+++ Suciu-Lavinia/L05/Tema5/Statistics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Models
+{
+    public class Statistics : TableEntity
+    {
+        public Statistics(string university, string date)
+        {
+            this.PartitionKey = university;
+            this.RowKey = date.ToString();
+        }
+        public Statistics() { }
+        public int TotalNrOfStudents { get; set; }
+    }
+}
